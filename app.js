a3e48aebe6f19c8533479ac994b4a543377230bc
taiki-engineer <taiki.m0209@outlook.jp>
2026-08-13 20:43:29 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const recordBtn = document.querySelector(".recordBtn");
+
+if (recordBtn) {
+    recordBtn.addEventListener("click", function () {
+
+        const exerciseName =
+            document.getElementById("exerciseName").value;
+
+        const weight =
+            document.getElementById("weight").value;
+
+        const reps =
+            document.getElementById("reps").value;
+
+        const sets =
+            document.getElementById("sets").value;
+
+        console.log("種目名:", exerciseName);
+        console.log("重量:", weight);
+        console.log("回数:", reps);
+        console.log("セット数:", sets);
+        
+        alert("トレーニングを記録しました！");
+
+    });
+}
